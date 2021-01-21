2b44ad39a096284729c93d6d6a4e3d183c41cdd1
Amanda Carpentieri <carpentieri.a@gmail.com>
2021-01-21 13:42:27 -0800

    added CRUD functionality

--- src/client/components/CreateChirp.tsx
+++ src/client/components/CreateChirp.tsx
@@ -5,12 +5,21 @@ import { RouteComponentProps } from 'react-router-dom';
 
 export interface createChirpProps extends RouteComponentProps<{ id: string }> { };
 
-const CreateChirp: React.FC<createChirpProps> = ({ history, match: { params: { id } } }) => {
+const CreateChirp: React.FC<createChirpProps> = ({ history }) => {
+
+    const [user, setUser] = useState('')
+    const [msg, setMessage] = useState('')
 
     const createChirp = async () => {
         const r = await fetch ('/api/chirps', {
-            method: 'POST'
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                user, 
+                msg
+            })
         })
+        .then(() => {history.push('/')})
     }
 
 
@@ -18,13 +27,13 @@ const CreateChirp: React.FC<createChirpProps> = ({ history, match: { params: { i
         <div className="container">
             <div className="form-group">
                 <label id="usernameLabel">Enter your Username</label>
-                <input type="text" className="form-control" />
+                <input id="username" type="text" className="form-control" onChange={event => setUser(event.target.value)} />
             </div>
             <div className="form-group">
                 <label id="chirpLabel">What Would You Like to Chirp?</label>
-                <textarea rows="3" className="form-control"></textarea>
+                <textarea rows="3" className="form-control" onChange={event => setMessage(event.target.value)}></textarea>
             </div>
-            <button id="createButton" type="button" className="btn mr-4">Post your Chirp</button>
+            <button id="createButton" type="button" className="btn mr-4" onClick={() => createChirp()}>Post your Chirp</button>
             <button id="createButton" type="button" className="btn" onClick={() => history.goBack()}> Go Back</button>
         </div>
     )
--- src/client/components/SingleChirp.tsx
+++ src/client/components/SingleChirp.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { IChirp } from './Chirps'
+import Swal from 'sweetalert2'
 
 export interface singlechirpProps extends RouteComponentProps<{ id: string }> { };
 
@@ -12,6 +13,7 @@ const singleChirp: React.FC<singlechirpProps> = ({ history, match: { params: { i
         msg: null
     });
 
+    //fetch api data for a single chirp
     const getsingleChirp = async () => {
         const r = await fetch(`/api/chirps/${id}`);
         const singleChirp = await r.json();
@@ -22,24 +24,53 @@ const singleChirp: React.FC<singlechirpProps> = ({ history, match: { params: { i
         getsingleChirp();
     }, [id])
 
+    //use fetch to delete a single chirp based on id
     const deleteChirp = async () => {
         const res = await fetch(`/api/chirps/${id}`, {
             method: 'delete'
-        });
-        return await res.json();
+        })
+        .then(() => {
+            Swal.fire({
+                title: `Chirp #${id} Deleted!`,
+                icon: 'success',
+                timer: 1500
+            })
+        })
+        .then(() => {history.push('/')})
+        .catch(err => {
+            alert('Error: Chirp not deleted');
+            console.log(err)
+        })
     }
 
+    //use fetch and useState Hook to edit a chirp
+    const [user, setUser] = useState('')
+    const [msg, setMessage] = useState('')
+
+    const editChirp = async () => {
+        const r = await fetch (`/api/chirps/${id}`, {
+            method: 'PUT',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                user, 
+                msg
+            })
+        })
+        .then(() => {history.push('/')})
+    }
+
+    //return card html to display chirp data
     return (
         <div className="container">
             <div className="form-group">
                 <label id="usernameLabel">Edit your Username</label>
-                <input type="text" className="form-control" placeholder={chirp?.user} />
+                <input type="text" className="form-control" placeholder={chirp?.user} onChange={event => setUser(event.target.value)}/>
             </div>
             <div className="form-group">
                 <label id="chirpLabel">What Would You Like to Edit?</label>
-                <textarea rows="3" className="form-control" placeholder={chirp?.msg}></textarea>
+                <textarea rows="3" className="form-control" placeholder={chirp?.msg} onChange={event => setMessage(event.target.value)}></textarea>
             </div>
-            <button id="chirpButton" className="btn-sm mr-4"> Save Edit</button>
+            <button id="chirpButton" className="btn-sm mr-4" onClick={() => editChirp()}> Save Edit</button>
             <button id="chirpButton" className="btn-sm mr-4" onClick={() => deleteChirp()}> Delete Chirp</button>
             <button id="chirpButton" className="btn-sm" onClick={() => history.goBack()}> Go Back</button>
         </div>
