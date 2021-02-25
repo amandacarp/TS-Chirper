1f29dcb319a914a8fe4b8ee9fa5965a4c11558cb
Amanda Carpentieri <carpentieri.a@gmail.com>
2021-02-25 15:45:43 -0800

    deploy

--- src/server/db/queries/chirpstable.ts
+++ src/server/db/queries/chirpstable.ts
@@ -1,8 +1,8 @@
 import { Query } from '../index';
 import {Chirp, SQLRes, User} from '../../../common/types'
 
-const all = () => Query<Chirp[]>('SELECT Chirps.*, Users.name FROM Chirps JOIN Users ON Users.id = Chirps.userid ORDER BY Chirps._created DESC');
-const by_id = async (id: Chirp["id"]) => Query<Chirp>('SELECT Chirps.*, Users.name FROM Chirps JOIN Users ON Users.id = Chirps.userid WHERE Chirps.id = ?', [id]);
+const all = () => Query<Chirp[]>('SELECT Chirps.*, users.name FROM Chirps JOIN users ON users.id = Chirps.userid ORDER BY Chirps._created DESC');
+const by_id = async (id: Chirp["id"]) => Query<Chirp>('SELECT Chirps.*, users.name FROM Chirps JOIN users ON users.id = Chirps.userid WHERE Chirps.id = ?', [id]);
 const delete_chirp = async (id: Chirp["id"], userid: User['id']) => Query('DELETE FROM Chirps WHERE id = ?', [id, userid]);
 const add_chirp = async (location: Chirp['location'], content: Chirp['content'], userid: User['id']) => Query<SQLRes>('INSERT INTO Chirps SET location = ?, content = ?, userid = ?', [location, content, userid]);
 const edit_chirp = async (content: Chirp['content'], id: Chirp['id'], userid: User['id']) => Query<SQLRes>('UPDATE Chirps SET content = ? WHERE id = ?', [content, id, userid])
--- src/server/db/queries/users.ts
+++ src/server/db/queries/users.ts
@@ -1,10 +1,10 @@
 import { Query } from '../';
 import {User, SQLRes} from '../../../common/types';
 
-const all = () => Query<User[]>('SELECT * FROM Users');
-const one = (id: User['id']) => Query<User[]>('SELECT * FROM Users WHERE id = ?', [id]);
-const insert = (newUser: User) => Query<SQLRes>('INSERT INTO Users SET ?', [newUser]);
-const find = (column: string, value: string | number) => Query<User[]>('SELECT * FROM Users WHERE ?? = ?', [column, value]);
+const all = () => Query<User[]>('SELECT * FROM users');
+const one = (id: User['id']) => Query<User[]>('SELECT * FROM users WHERE id = ?', [id]);
+const insert = (newUser: User) => Query<SQLRes>('INSERT INTO users SET ?', [newUser]);
+const find = (column: string, value: string | number) => Query<User[]>('SELECT * FROM users WHERE ?? = ?', [column, value]);
 
 
 
